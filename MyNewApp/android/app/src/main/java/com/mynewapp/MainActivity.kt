1330d9866dfd95d9afe0d95bcf432c3db7da12bc
4AQKKC <134270597+4AQKKC@users.noreply.github.com>
2025-08-15 17:59:37 +0700

    Update MainActivity.kt

--- MyNewApp/android/app/src/main/java/com/mynewapp/MainActivity.kt
+++ MyNewApp/android/app/src/main/java/com/mynewapp/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.mynewapp
 
 import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
     override fun getMainComponentName(): String = "MyNewApp"
-
-    override fun createReactActivityDelegate(): ReactActivityDelegate {
-        return DefaultReactActivityDelegate(this, mainComponentName)
-    }
 }
