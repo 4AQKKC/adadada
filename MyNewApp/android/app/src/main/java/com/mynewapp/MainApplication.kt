d8b633295a009bb9aa14d4d28b3c1276d7489719
4AQKKC <134270597+4AQKKC@users.noreply.github.com>
2025-08-15 12:29:09 +0700

    Update MainApplication.kt

--- MyNewApp/android/app/src/main/java/com/mynewapp/MainApplication.kt
+++ MyNewApp/android/app/src/main/java/com/mynewapp/MainApplication.kt
@@ -1,38 +1,49 @@
-package com.mynewapp
+package com.mynewapp // <-- sửa lại theo package app của bạn
 
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+import java.lang.reflect.InvocationTargetException
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            val packages = PackageList(this).packages
+            // Nếu bạn có FloatingOverlayPackage tự viết, thêm dưới đây:
+            // packages.add(FloatingOverlayPackage())
+            return packages
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, /* native exopackage */ false)
+        if (BuildConfig.DEBUG) {
+            try {
+                val a = Class.forName("com.facebook.react.ReactNativeFlipper")
+                a.getMethod("initializeFlipper", Application::class.java)
+                    .invoke(null, this)
+            } catch (e: ClassNotFoundException) {
+            } catch (e: NoSuchMethodException) {
+            } catch (e: IllegalAccessException) {
+            } catch (e: InvocationTargetException) {
             }
-
-        override fun getJSMainModuleName(): String = "index"
-
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+        }
+    }
 }
